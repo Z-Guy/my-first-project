519d20cd796929e5e2842ec94d77b15b2de6be84
Christian Tsegaye <C.Tsegaye@outlook.com>
2023-11-12 14:40:39 -0500

    Add HTTP communication to your app

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -33,9 +33,11 @@ export class HomeComponent {
   filteredLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
 
-  constructor(){
-    this.housingLocationList = this.housingService.getAllHousingLocations();
-    this.filteredLocationList = this.housingLocationList;
+  constructor() {
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
   }
 
   filterResults(text: string) {
