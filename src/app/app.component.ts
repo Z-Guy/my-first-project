c9c34b4135f54f84c4b3b885afcd7164bcfd4c58
Christian Tsegaye <C.Tsegaye@outlook.com>
2023-11-12 02:39:15 -0500

    The start and added home components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  imports: [HomeComponent,],
+  styleUrls: ['./app.component.css'],
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+`,
 })
 export class AppComponent {
-  title = 'my-first-project';
+  title = 'homes';
 }
